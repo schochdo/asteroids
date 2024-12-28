46c4d9c5969d511c0354ae9d66c019fd69dea989
schochdo <schochdo@comcast.net>
2024-12-27 18:25:37 -0700

    edited to match solution

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from pygame.locals import *
 from constants import *
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	print("Starting asteroids!")
-	print(f"Screen width: {SCREEN_WIDTH}")
-	print(f"Screen height: {SCREEN_HEIGHT}")
+	
+	clock = pygame.time.Clock()
+	dt = 0
+	
 
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
-		pygame.Surface.fill(screen, (0,0,0,9))
+		screen.fill("black")
 		pygame.display.flip()
-
+		# 60 FPS
+		dt = clock.tick(60) / 1000
+	
 if __name__ == "__main__":
 	main()

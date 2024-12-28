050eebc7e5bf54c24bc63ea205fbfa1ee7f597a0
schochdo <schochdo@comcast.net>
2024-12-27 19:28:39 -0700

    added player triangle

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	
 	clock = pygame.time.Clock()
 	dt = 0
-	
+
+	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
 	while True:
 		for event in pygame.event.get():
     			if event.type == pygame.QUIT:
         			return
+
 		screen.fill("black")
+		player.draw(screen)
 		pygame.display.flip()
+
 		# 60 FPS
 		dt = clock.tick(60) / 1000
-	
+
 if __name__ == "__main__":
 	main()

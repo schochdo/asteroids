da696e09e07acfc44ec8716b238d4f95d7cbca73
schochdo <schochdo@comcast.net>
2024-12-29 19:37:10 -0700

    added asteroids

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
-	pygame.init()
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	clock = pygame.time.Clock()
-	dt = 0
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
-	player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-	while True:
-		for event in pygame.event.get():
-    			if event.type == pygame.QUIT:
-        			return
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
-		screen.fill("black")
-		player.update(dt)
-		player.draw(screen)
-		pygame.display.flip()
+    Player.containers = (updatable, drawable)
 
-		# 60 FPS
-		dt = clock.tick(60) / 1000
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-	main()
+    main()
